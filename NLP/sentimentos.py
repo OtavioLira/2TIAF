401db8ef18ce01f6806509b77f0be8c79bd6b9f1
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-09 11:49:19 -0300

    NLP: Desafio analise de sentimentos com blob

--- NLP/sentimentos.py
+++ NLP/sentimentos.py
@@ -1,6 +1,10 @@
+import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
+nltk.download('vader_lexicon')
+
 analyzer = SentimentIntensityAnalyzer()
+
 
 text = "I love this product! It's amazing."
 scores = analyzer.polarity_scores(text)
